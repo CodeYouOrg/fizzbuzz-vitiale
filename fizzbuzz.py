de3d5ddb795165c5b3dc24dd28d1e7438320842b
vitiale <vlirianoluis@gmail.com>
2024-07-21 07:03:17 -0400

    Comments rest of the code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,17 @@
 # add your code here
 
+for i in range(1,100):
+            if i % 3 == 0 and i % 5 == 0 :
+                print('FizzBuzz')
+            elif i % 3 == 0 :
+                print('Fizz')
+            elif i % 5 == 0 :
+                print('Buzz')
+            else:
+                print(i)
 
+#Code Module 1-4 (FizzBuzz) Adding Complexity
+"""
 def fizzbuzzFunction(a, b):
     if a < b :
         for i in range(a,b):
@@ -26,6 +37,6 @@ try:
 except ValueError:
 
     print("Invalid input. Please enter valid integers")    
-
+"""
         
 
